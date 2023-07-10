1ef94d57c30ae9d1cbfd6bdcfd429b396ca35774
Jorge <J4ck.DevCode@gmail.com>
2023-07-09 20:22:51 -0500

    Added .env implementation and file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:comic_app/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
